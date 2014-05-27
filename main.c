01e0ce2ee20ea8b04b8da8ad526fde53b2f594d7
Jenny Wong <jy2wong@csclub.uwaterloo.ca>
2014-05-27 00:04:40 -0400

    replace GtkDrawingArea with GtkImage

--- main.c
+++ main.c
@@ -18,19 +18,8 @@ static void prefs_cb(GtkMenuItem *menuitem, GtkDialog *prefs) {
   }
   gtk_widget_destroy(GTK_WIDGET(prefs));
 }
-static gboolean canvas_draw_cb(GtkWidget *widget, cairo_t *cr, gpointer data) {
-  guint width, height;
-  width = gtk_widget_get_allocated_width (widget);
-  height = gtk_widget_get_allocated_height (widget);
-  cairo_set_source_rgba (cr, 1, 0.2, 0.2, 0.6);
-  cairo_arc (cr,
-                   width / 2.0, height / 2.0,
-                                MIN (width, height) / 2.0,
-                                             0, 2 * G_PI);
-  return TRUE;
-}
+
 static void spektro_startup_cb(GtkApplication *app, gpointer user_data) {
-  g_warning("startup");
   gtk_application_set_menubar(GTK_APPLICATION(app), NULL);
   gtk_application_set_app_menu(GTK_APPLICATION(app), NULL);
   return;
@@ -50,11 +39,9 @@ static void spektro_activate_cb(GtkApplication *app, gpointer user_data) {
   GtkScrolledWindow *scroll =GTK_SCROLLED_WINDOW(
       gtk_builder_get_object(builder, "scrolledwindow"));
 
-  GtkDrawingArea *canvas = GTK_DRAWING_AREA(gtk_builder_get_object(builder,
-                                                                   "canvas"));
-  g_signal_connect(G_OBJECT(canvas), "draw", G_CALLBACK(canvas_draw_cb), NULL);
+  GtkImage *canvas = GTK_IMAGE(gtk_builder_get_object(builder, "canvas"));
 
-  // open preferences window
+  // open preferences window with menu
   GtkDialog *prefs = GTK_DIALOG(gtk_builder_get_object(builder, "preferences-dialog"));
   GtkMenuItem *menu_prefs = GTK_MENU_ITEM(gtk_builder_get_object(builder, "menu-edit-prefs"));
   g_signal_connect(G_OBJECT(menu_prefs), "activate", G_CALLBACK(prefs_cb), prefs);
@@ -64,11 +51,7 @@ static void spektro_activate_cb(GtkApplication *app, gpointer user_data) {
   GtkMenuItem *menu_about = GTK_MENU_ITEM(gtk_builder_get_object(builder, "menu-help-about"));
   g_signal_connect(G_OBJECT(menu_about), "activate", G_CALLBACK(about_cb), about);
 
-  //gtk_grid_attach(grid, GTK_WIDGET(label), 0, 0, 1, 1);
-  //gtk_widget_show(GTK_WIDGET(label));
-
   //gtk_widget_show(GTK_WIDGET(app_window));
-  g_warning("activate10");
   return;
 }
 
